564d97cdf73db071f4bdb5fe63b6d6a2c316c2a1
Tanmay <tanmaytanvi1234@gmail.com>
2026-01-30 17:19:41 +0530

    [Tanmay] : Added UC6: Introduce Custom

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -3,18 +3,10 @@ import java.util.Scanner;
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        RobotHazardAuditor auditor = new RobotHazardAuditor();
-
-        double armPrecision = sc.nextDouble();
-        int workerDensity = sc.nextInt();
-        sc.nextLine();
-        String machineryState = sc.nextLine();
-
-        double risk = auditor.calculateHazardRisk(
-                armPrecision, workerDensity, machineryState);
-
-        System.out.println("Robot Hazard Risk Score: " + risk);
+        try {
+            throw new RobotSafetyException("Safety violation detected");
+        } catch (RobotSafetyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
