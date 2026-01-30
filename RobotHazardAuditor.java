c63680230c810a107a848186517ad0fd782cd74b
Tanmay <tanmaytanvi1234@gmail.com>
2026-01-30 17:44:22 +0530

    [Tanmay] : Added UC7: Machinery State Risk Mapping + Validation

--- RobotHazardAuditor.java
+++ RobotHazardAuditor.java
@@ -2,9 +2,24 @@ public class RobotHazardAuditor {
 
     public double calculateHazardRisk(double armPrecision,
                                       int workerDensity,
-                                      String machineryState) {
+                                      String machineryState)
+            throws RobotSafetyException {
 
-        double machineRiskFactor = 3.0;
+        double machineRiskFactor;
+
+        if (machineryState.equals("Worn")) {
+            machineRiskFactor = 1.3;
+        }
+        else if (machineryState.equals("Faulty")) {
+            machineRiskFactor = 2.0;
+        }
+        else if (machineryState.equals("Critical")) {
+            machineRiskFactor = 3.0;
+        }
+        else {
+            throw new RobotSafetyException(
+                    "Error: Unsupported machinery state");
+        }
 
         return ((1.0 - armPrecision) * 15.0)
                 + (workerDensity * machineRiskFactor);
