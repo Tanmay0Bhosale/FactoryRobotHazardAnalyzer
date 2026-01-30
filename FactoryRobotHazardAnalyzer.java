13ff5f739da6c41cdd2b503d16b68e0c5a4c2afa
Tanmay <tanmaytanvi1234@gmail.com>
2026-01-30 16:15:45 +0530

    [Tanmay] : Added UC3: Calculate Hazard Risk Score (No Validation)

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -11,13 +11,17 @@ public class FactoryRobotHazardAnalyzer {
 
         System.out.println("Enter Worker Density (1 - 20):");
         int workerDensity = sc.nextInt();
-        sc.nextLine(); // consume newline
+        sc.nextLine();
 
         System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
         String machineryState = sc.nextLine();
 
-        System.out.println("Arm Precision: " + armPrecision);
-        System.out.println("Worker Density: " + workerDensity);
-        System.out.println("Machinery State: " + machineryState);
+        double machineRiskFactor = 3.0; // assume Critical
+
+        double hazardRisk =
+                ((1.0 - armPrecision) * 15.0)
+                        + (workerDensity * machineRiskFactor);
+
+        System.out.println("Robot Hazard Risk Score: " + hazardRisk);
     }
 }
